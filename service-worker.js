490fcd7627f9a054a941d412b4d22448d0333eb4
Yousif <yousifiraqas5@gmail.com>
2026-09-22 10:18:38 +0300

    Fix service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hamoulati-v3";
+﻿const CACHE_NAME = "hamoulati-v4";
 
 const FILES_TO_CACHE = [
     "./",
@@ -17,6 +17,19 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
     );
 });
 
